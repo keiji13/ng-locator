2f67ee57b873ab705cce6fdd330cc67b4ea9d798
keiji13 <jconaco@yahoo.com>
2016-03-11 12:45:12 +0800

    First commit

---
+++ locator-directive.js
@@ -0,0 +1,30 @@
+function ngLocator(Locator){
+  var Link = function(scope, element, attrs){
+    scope.location = {};
+    
+    function init(location){
+      console.log(location);
+      scope.location.coords = location;
+      var mapContainer = element[0].getElementsByClassName('locator-map')[0],
+          autoCompleteInput = element[0].getElementsByClassName('locator-autocomplete')[0];
+    }
+    
+    Locator.getLocation().then(init);
+  };
+  
+  return {
+    restrict: 'E',
+    templateUrl: 'locator.html',
+    scope: {
+      location: '='
+    },
+    link: Link,
+    controller: angular.noop,
+    controllerAs: 'locator'
+  };
+}
+
+ngLocator.$injects = ['Locator'];
+
+angular.module('locator')
+.directive('ngLocator', ngLocator);
